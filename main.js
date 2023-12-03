e99b796643aefc4b57372389aeb83f484fc81f93
Caique Guedes <caique.guedes@estudante.ufjf.br>
2023-12-03 18:07:46 -0300

    corrige vetor e readiciona novas funcoes

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
+import { getLista } from "./lista";
 
-
+function criaElementoNaLista(){
+    const texto = pEntrada.textContent;
+    adicionanaLista(texto);
+    atualizarItensDeLista();
+    pEntrada.textContent="";
+    pEntrada.focus();
+}
 
 
 function atualizarItensDeLista(){
     olSaida.innerHTML = "";
     const lista = getLista();
-    for(let i = 0; i < lista.length; < i++){
-        
+    for(let i = 0; i < lista.length;i++){
+     const item = lista[i];
+     const li = document.createElement("li");
+     olSaida.appendChild(li);   
     }
 }
